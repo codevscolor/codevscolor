166a58e5c5b48489c5e97374447808bb32ec71a1
CodeVsColor <codevscolor@gmail.com>
2019-02-11 21:46:10 -0800

    Create find_simple_interest.py

---
+++ python/find_simple_interest.py
@@ -0,0 +1,7 @@
+#1
+P = float(input("Enter the principal amount : "))
+N = float(input("Enter the number of years : "))
+R = float(input("Enter the rate of interest : "))
+#2
+SI = (P * N * R)/100
+print("Simple interest : {}".format(SI))

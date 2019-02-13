27df69922e1c4bbad40b818a90077adf9c614525
CodeVsColor <codevscolor@gmail.com>
2019-02-12 19:15:03 -0800

    Create multiplication_of_two_nos.py

---
+++ python/multiplication_of_two_nos.py
@@ -0,0 +1,7 @@
+#1
+first_number = int(input("Enter your first number : "))
+second_number = int(input("Enter your second number : "))
+#2
+final_result = first_number * second_number
+#3
+print("Multiplication of {} and {} is : {}".format(first_number,second_number,final_result))

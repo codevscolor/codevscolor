00c0ae3f319daa05db7cbc2198995e80c0f8321e
CodeVsColor <codevscolor@gmail.com>
2019-02-12 19:12:26 -0800

    Create set_intersection.py

---
+++ python/set_intersection.py
@@ -0,0 +1,9 @@
+firstSet = {1,2,3}
+secondSet = {2,4,5}
+thirdSet = {1,10,2,4}
+print("firstSet : ",firstSet)
+print("secondSet : ",secondSet)
+print("thirdSet : ",thirdSet)
+print("firstSet ∩ secondSet : ",firstSet.intersection(secondSet))
+print("firstSet ∩ thirdSet : ",firstSet.intersection(thirdSet))
+print("firstSet ∩ secondSet ∩ thirdSet : ",firstSet.intersection(secondSet,thirdSet))

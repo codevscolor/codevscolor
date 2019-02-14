369238c2408a8ba7df1ae40a44d3befbb5f6e972
CodeVsColor <codevscolor@gmail.com>
2019-02-13 20:55:14 -0800

    Create find_length_str.py

---
+++ python/find_length_str.py
@@ -0,0 +1,20 @@
+#method 1
+
+#1
+given_str = "Hello World"
+
+#2
+size = 0
+
+#3
+for c in given_str :
+    size = size + 1
+
+#4
+print("Size of the given string is : ",size)
+
+#method 2
+
+given_str = "Hello World"
+
+print("Size of the given string is : ",len(given_str))

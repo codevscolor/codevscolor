201526a41a94622f1b8d0994feed13c54da08701
CodeVsColor <codevscolor@gmail.com>
2019-02-18 10:41:31 +0530

    Create replace_character_with_symbol.py

---
+++ python/replace_character_with_symbol.py
@@ -0,0 +1,5 @@
+input_string = str(input("Enter a string : ")) //1
+c = input("Enter a character you want to modify in the above string : ") //2
+symbol = input("Enter the symbol you want to replace with : ") //3
+modified_str = input_string.replace(c,symbol) //4
+print("Modified String is : ",modified_str) //5

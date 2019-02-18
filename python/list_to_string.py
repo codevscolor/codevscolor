8585cfb9631f0f915b2eaa768e40963b099244b3
CodeVsColor <codevscolor@gmail.com>
2019-02-18 09:46:48 +0530

    Update list_to_string.py

--- python/list_to_string.py
+++ python/list_to_string.py
@@ -1,6 +1,18 @@
+#example 1
+
 dummy_list = ["one","two","three","four","five","six"]
 
 separator = ' '
+
+result_string = separator.join(dummy_list)
+
+print(result_string)
+
+#example 2
+
+dummy_list = ["one","two","three","four","five","six"]
+
+separator = ','
 
 result_string = separator.join(dummy_list)
 

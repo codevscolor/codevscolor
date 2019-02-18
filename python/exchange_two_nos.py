5f137cadc9d0de624ca59af3eea163ec20fa5577
CodeVsColor <codevscolor@gmail.com>
2019-02-18 10:16:40 +0530

    Create exchange_two_nos.py

---
+++ python/exchange_two_nos.py
@@ -0,0 +1,11 @@
+#1
+first = int(input("Enter the value of the first number : "))
+second = int(input("Enter the value of the second number : "))
+#2
+first = first + second
+#3
+second = first - second
+#4
+first = first - second 
+#5
+print("After exchange, First number is : ",first," Second number is : ",second)

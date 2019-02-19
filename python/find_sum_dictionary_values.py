544ad9a1d5a11002ac173c8294eb6a61f40262a1
CodeVsColor <codevscolor@gmail.com>
2019-02-19 10:08:58 +0530

    Create find_sum_dictionary_values.py

---
+++ python/find_sum_dictionary_values.py
@@ -0,0 +1,8 @@
+my_dictionary = {'1' : 1,'2' : 2,'3' : 3,'4' : 4,'5' : 5}
+
+print("Given Dictionary : ",my_dictionary)
+print("values ",my_dictionary.values())
+
+sum_values = sum(my_dictionary.values())
+
+print("Sum of all the values in the dictionary is : ",sum_values)

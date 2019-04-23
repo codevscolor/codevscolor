4708b22f629ac5589f3d5e28b5c1315659b6a105
CodeVsColor <codevscolor@gmail.com>
2019-04-23 12:50:01 +0530

    Create concatenate_two_dictionaries.py

---
+++ python/concatenate_two_dictionaries.py
@@ -0,0 +1,6 @@
+dict_first = {'One':1, 'Two':2, 'Three':3, 'Four':4, 'Five':5}
+dict_second = {'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10}
+
+dict_first.update(dict_second)
+
+print("Final dictionary after concatenation : ",dict_first)

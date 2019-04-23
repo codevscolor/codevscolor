da6802eb5618800699c37b33b6469be165f99d07
CodeVsColor <codevscolor@gmail.com>
2019-04-23 10:32:11 +0530

    Create print_triangle_star.py

---
+++ python/print_triangle_star.py
@@ -0,0 +1,13 @@
+#program 1
+height = int(input("Enter the height of the triangle : "))
+
+for i in range(height,0,-1):
+    print(i* ' ' + (height+1-i) * '*')
+    
+    
+    
+#program 2
+height = int(input("Enter the height of the triangle : "))
+
+for i in range(height,0,-1):
+    print(i* 'k' + (height+1-i) * '*')

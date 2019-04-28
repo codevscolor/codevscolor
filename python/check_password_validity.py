355147ed3d30b454c7381bd4f44e66cbfaeb20a5
CodeVsColor <codevscolor@gmail.com>
2019-04-28 18:39:41 +0530

    Create check_password_validity.py

---
+++ python/check_password_validity.py
@@ -0,0 +1,41 @@
+#1
+import re 
+
+#2
+while True:
+	#3
+	user_input = input("Enter a password : ")
+	is_valid = False 
+
+	if (len(user_input)<6 or len(user_input)>12):
+		#4
+		print("Not valid ! Total characters should be between 6 and 12")
+		continue
+	elif not re.search("[A-Z]",user_input):
+		#5
+		print("Not valid ! It should contain one letter between [A-Z]")
+		continue
+	elif not re.search("[a-z]",user_input):
+		#6
+		print("Not valid ! It should contain one letter between [a-z]")
+		continue
+	elif not re.search("[1-9]",user_input):
+		#7
+		print("Not valid ! It should contain one letter between [1-9]")
+		continue
+	elif not re.search("[~!@#$%^&*]",user_input):
+		#8
+		print("Not valid ! It should contain at least one letter in [~!@#$%^&*]")
+		continue
+	elif re.search("[\s]",user_input):
+		#9
+		print("Not valid ! It should not contain any space")
+		continue
+	else:
+		#10
+		is_valid = True
+		break
+
+#11
+if(is_valid):
+	print("Password is valid")

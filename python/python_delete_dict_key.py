38b4fe4286f59c116ee83a321093d35415e4dceb
CodeVsColor <codevscolor@gmail.com>
2019-04-29 10:18:02 +0530

    Create python_delete_dict_key.py

---
+++ python/python_delete_dict_key.py
@@ -0,0 +1,17 @@
+#1
+dict = {'one':1,'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7,'eight':8,'nine':9,'ten':10}
+
+#2
+print("Current Dictionary : ",dict)
+
+#3
+key_to_delete = input("Enter the key to delete : ")
+
+#4
+if key_to_delete in dict:
+	del dict[key_to_delete]
+else :
+	print("Please enter a valid key.")
+
+#5
+print("Final Dictionary ",dict)

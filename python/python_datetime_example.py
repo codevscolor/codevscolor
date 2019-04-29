1cb91e6651407c93e034dbd9544b8c86e1eb2b26
CodeVsColor <codevscolor@gmail.com>
2019-04-29 07:50:35 +0530

    Create python_datetime_example.py

---
+++ python/python_datetime_example.py
@@ -0,0 +1,32 @@
+import datetime
+
+def currentTime():
+	print("Current date and time : ")
+	today = datetime.datetime.strftime(datetime.datetime.today() , '%d/%m/%Y-%Hh/%Mm')
+	print(today)
+
+oneDayLater = datetime.datetime.today() + datetime.timedelta(days = 1)
+currentTime()
+print("Time after one day : ")
+print(datetime.datetime.strftime(oneDayLater , '%d/%m/%Y-%Hh/%Mm'))
+
+
+fourWeeksLater = datetime.datetime.today() + datetime.timedelta(weeks = 4)
+print("")
+currentTime()
+print("Time after four weeks : ")
+print(datetime.datetime.strftime(fourWeeksLater , '%d/%m/%Y-%Hh/%Mm'))
+
+
+oneHourLater = datetime.datetime.today() + datetime.timedelta(hours = 1)
+print("")
+currentTime()
+print("Time after one hour : ")
+print(datetime.datetime.strftime(oneHourLater , '%d/%m/%Y-%Hh/%Mm'))
+
+
+minutesLater = datetime.datetime.today() + datetime.timedelta(minutes = 15)
+print("")
+currentTime()
+print("Time after 15 minutes : ")
+print(datetime.datetime.strftime(minutesLater , '%d/%m/%Y-%Hh/%Mm'))

0a00fdb8408b9ee1c5320e1d3966f2340abe45bf
CodeVsColor <codevscolor@gmail.com>
2019-04-29 23:06:26 +0530

    Create python_range.py

---
+++ python/python_range.py
@@ -0,0 +1,52 @@
+# example 1
+for i in range(10):
+	print(i)
+  
+  
+# example 2
+my_list = [1,2,3,4,5]
+my_range = range(5)
+
+print("Type of my_list : ",type(my_list))
+print(my_list)
+
+print("Type of my_range : ",type(my_range))
+print(my_range)
+
+
+# example 3
+my_list = list(range(5))
+
+print("Type of my_list : ",type(my_list))
+print(my_list)
+
+
+# example 4
+my_list = [1,2,3,4,5]
+
+for i in range(len(my_list)):
+	print(my_list[i])
+  
+  
+# example 5
+for i in range(5,10):
+	print(i)
+  
+  
+# example 6
+for i in range(5,10,2):
+	print(i)
+  
+  
+# example 7
+my_list = [1,2,3,4,5,6,7,8,9,10]
+
+for i in range(5,10):
+	print(my_list[i])
+    
+  
+# example 8
+my_list = [1,2,3,4,5,6,7,8,9,10]
+
+for i in range(3,10,2):
+	print(my_list[i])

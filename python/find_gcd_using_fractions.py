4eebbc8056f5b640b45626c391256dfd83293214
CodeVsColor <codevscolor@gmail.com>
2019-05-12 08:38:43 +0530

    Create find_gcd_using_fractions.py

---
+++ python/find_gcd_using_fractions.py
@@ -0,0 +1,7 @@
+//1
+import fractions
+//2
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : "))
+//3
+print("The gcd of {} and {} is {}".format(num1, num2, fractions.gcd(num1, num2)))

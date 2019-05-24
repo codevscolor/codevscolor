59c46b0fbd137db2874a392ad66f2fa57ded01e3
CodeVsColor <codevscolor@gmail.com>
2019-05-24 08:32:57 +0530

    Create example2.py

---
+++ python/set-difference/example2.py
@@ -0,0 +1,18 @@
+    setA = set()
+    setB = set()
+    setA_length = int(input(“Enter the size of the first set : “))
+    setB_length = int(input(“Enter the size of the second set : “))
+    print(“\n”)
+    print(“Enter values for the first set : \n”)
+    for i in range(setA_length):
+        e = int(input(“Enter value {} : “.format(i+1)))
+        setA.add(e)
+    print(“\n”)
+    print(“Enter values for the second set : \n”)
+    for i in range(setB_length):
+        e = int(input(“Enter value {} : “.format(i+1)))
+        setB.add(e)
+    print(“\n”)
+    print(“First set : {}”.format(setA))
+    print(“Second set : {}”.format(setB))
+    print(“Difference : {}”.format(setA - setB))

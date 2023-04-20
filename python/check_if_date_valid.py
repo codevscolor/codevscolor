861fdd7d3b66cde5d0e1aaa2f8af74541b9a89c6
codevscolor <codevscolor@gmail.com>
2023-04-20 14:09:57 +0530

    resetting file

---
+++ python/check_if_date_valid.py
@@ -0,0 +1,16 @@
+import datetime
+
+inputDate = input("Enter the date in format 'dd/mm/yy' : ")
+
+day,month,year = inputDate.split('/')
+
+isValidDate = True
+try :
+    datetime.datetime(int(year),int(month),int(day))
+except ValueError :
+    isValidDate = False
+
+if(isValidDate) :
+    print ("Input date is valid ..")
+else :
+    print ("Input date is not valid..")

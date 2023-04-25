87ec93d9184afa22c2e8bd624d01272061c34aa1
codevscolor <codevscolor@gmail.com>
2023-04-25 16:45:34 +0530

    python

---
+++ python/python-add-items-list-append/example1.py
@@ -0,0 +1,9 @@
+# https://codevscolor.com/python-add-items-list-append
+given_list = []
+given_list += [1]
+given_list += [2]
+given_list += [3]
+given_list += [4]
+given_list += [5, 6, 7, 8]
+
+print(given_list)
---
+++ python/python-add-items-list-append/example3.py
@@ -0,0 +1,7 @@
+# https://codevscolor.com/python-add-items-list-append
+given_list = [1, 2, 3, 4, 5]
+size = len(given_list)
+
+given_list[size:] = [6, 7, 8]
+
+print(given_list)

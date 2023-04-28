092136c7e06e2ce56246d73e3a718802d0e4abdc
codevscolor <codevscolor@gmail.com>
2023-04-28 20:33:01 +0530

    python

---
+++ python/python-count-words-characters-string/example1.py
@@ -0,0 +1,21 @@
+# 1
+word_count = 0
+char_count = 0
+
+# 2
+usr_input = input("Enter a string : ")
+
+# 3
+split_string = usr_input.split()
+
+# 4
+word_count = len(split_string)
+
+# 5
+for word in split_string:
+    # 6
+    char_count += len(word)
+
+# 7
+print("Total words : {}".format(word_count))
+print("Total characters : {}".format(char_count))
---
+++ python/python-count-words-characters-string/example2.py
@@ -0,0 +1,12 @@
+word_count = 0
+char_count = 0
+last_word_space = False
+
+usr_input = input("Enter a string : ")
+word_list = usr_input.split()
+
+word_count = len(word_list)
+char_count = sum([len(c) for c in word_list])
+
+print("Total words : {}".format(word_count))
+print("Total characters : {}".format(char_count))

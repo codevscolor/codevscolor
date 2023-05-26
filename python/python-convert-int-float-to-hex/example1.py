f4d2413e4fd041a52eb252872268ed7dcf1b11a6
codevscolor <codevscolor@gmail.com>
2023-05-26 19:47:40 +0530

    python

---
+++ python/python-convert-int-float-to-hex/example1.py
@@ -0,0 +1,7 @@
+# https://codevscolor.com/python-convert-int-float-to-hex
+
+print("Hex value of 2 is ",hex(2))
+print("Hex value of 3 is ",hex(3))
+print("Hex value of 16 is ",hex(16))
+print("Hex value of 7 is ",hex(7))
+print("Hex value of 21 is ",hex(21))
---
+++ python/python-convert-int-float-to-hex/example2.py
@@ -0,0 +1,6 @@
+# https://codevscolor.com/python-convert-int-float-to-hex
+
+print("Hex value of 2.4 is ",float.hex(2.4))
+print("Hex value of 21.14 is ",float.hex(21.14))
+print("Hex value of 2.41 is ",float.hex(2.41))
+print("Hex value of 10.4 is ",float.hex(10.4))

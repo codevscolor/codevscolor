279b37ca79cde2eca95bc88f8b842b5f7395716e
codevscolor <codevscolor@gmail.com>
2023-05-29 10:38:27 +0530

    swift

---
+++ swift/swift-find-first-last-character-string/Example1.swift
@@ -0,0 +1,10 @@
+// https://codevscolor.com/swift-find-first-last-character-string
+let givenString = "hello world"
+
+if let firstChar = givenString.first{
+    print(firstChar)
+}
+
+if let lastChar = givenString.last{
+    print(lastChar)
+}
---
+++ swift/swift-find-first-last-character-string/Example2.swift
@@ -0,0 +1,5 @@
+// https://codevscolor.com/swift-find-first-last-character-string
+let givenString = "hello world"
+
+print(givenString[givenString.startIndex])
+print(givenString[givenString.index(before: givenString.endIndex)])

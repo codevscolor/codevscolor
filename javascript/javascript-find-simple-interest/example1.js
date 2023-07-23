4afc57f785a3a9eb39910eee5196c59c6250228c
codevscolor <codevscolor@gmail.com>
2023-07-23 10:12:41 +0530

    js

---
+++ javascript/javascript-find-simple-interest/example1.js
@@ -0,0 +1,8 @@
+// https://codevscolor.com/javascript-find-simple-interest
+const p = 1200;
+const n = 5;
+const r = 0.2;
+
+const simpleInterest = (p * n * r) / 100;
+
+console.log(`Simple Interest: ${simpleInterest}`);

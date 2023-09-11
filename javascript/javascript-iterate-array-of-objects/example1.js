072410371f0af440ee146b13216c4eaefee4cd8f
codevscolor <codevscolor@gmail.com>
2023-09-11 22:25:19 +0530

    js

---
+++ javascript/javascript-iterate-array-of-objects/example1.js
@@ -0,0 +1,7 @@
+// https://codevscolor.com/javascript-iterate-array-of-objects
+
+const arr = [{id: 1, name: 'Alex', age: 20}, {id: 2, name: 'Bob', age: 22}, {id: 3, name: 'Chandler', age: 21}];
+
+for(let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
---
+++ javascript/javascript-iterate-array-of-objects/example2.js
@@ -0,0 +1,10 @@
+// https://codevscolor.com/javascript-iterate-array-of-objects
+
+const arr = [{ id: 1, name: 'Alex', age: 20 }, { id: 2, name: 'Bob', age: 22 }, { id: 3, name: 'Chandler', age: 21 }];
+
+let i = 0;
+
+while (i < arr.length) {
+    console.log(arr[i]);
+    i++;
+};
